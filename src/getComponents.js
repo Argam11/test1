16a342ffe960aa881d5c8288c25d97f4ba17e1e5
unknown <argam.markosyan1994@gmail.com>
2019-09-29 19:24:49 +0400

    test 2

--- src/getComponents.js
+++ src/getComponents.js
@@ -2,10 +2,35 @@ import * as componentsImports from "./componentsImports";
 import componentsList from "./componentsList";
 import helper from "./helper";
 
-export default function(firstValue = 1, secondValue = 1) {
-    const hasComponent = helper(componentsList, ...arguments);
+export default function(
+    firstValue = 1,
+    secondValue = 1,
+    thirdValue = 1,
+    fourthValue = 1
+) {
+    let x = true;
+    let newArgs = Object.values(arguments).filter(args => {
+        !args && (x = false);
+        return x && args;
+    });
+    const hasComponent = helper(componentsList, ...newArgs);
 
     if (hasComponent) {
-        return componentsImports[componentsList[firstValue][secondValue]];
+        switch (newArgs.length) {
+            case 2:
+                return componentsImports[
+                    componentsList[firstValue][secondValue]
+                ];
+            case 3:
+                return componentsImports[
+                    componentsList[firstValue][secondValue][thirdValue]
+                ];
+            case 4:
+                return componentsImports[
+                    componentsList[firstValue][secondValue][thirdValue][
+                        fourthValue
+                    ]
+                ];
+        }
     }
 }
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,19 @@
 export default function(components, ...args) {
-    return !!(components[args[0]] && components[args[0]][args[1]]);
+    switch (args.length) {
+        case 2:
+            return !!(components[args[0]] && components[args[0]][args[1]]);
+        case 3:
+            return !!(
+                components[args[0]] &&
+                components[args[0]][args[1]] &&
+                components[args[0]][args[1]][args[2]]
+            );
+        case 4:
+            return !!(
+                components[args[0]] &&
+                components[args[0]][args[1]] &&
+                components[args[0]][args[1]][args[2]] &&
+                components[args[0]][args[1]][args[2]][args[3]]
+            );
+    }
 }
