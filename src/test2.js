fb0bdbe3c48af56ecc4956e8a887619758412dd8
unknown <argam.markosyan1994@gmail.com>
2019-10-24 00:46:59 +0400

    test stash command

--- src/test2.js
+++ src/test2.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 function Test2({ x, y }) {
-
-
-    
     return (
         <div className="Test flex flex-col items-center font-bold text-red">
             <h1>Test2</h1>
