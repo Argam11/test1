fa504ba06cf03d93991890ffc8cf9a9c9b91daa8
unknown <argam.markosyan1994@gmail.com>
2019-10-23 23:30:32 +0400

    installing tailwindcss

--- src/test2.js
+++ src/test2.js
@@ -4,7 +4,7 @@ function Test2({ x, y }) {
     console.log(222222);
 
     return (
-        <div className="Test">
+        <div className="Test flex flex-col items-center font-bold text-red">
             <h1>Test2</h1>
             <p>{x}</p>
             <p>{y}</p>
