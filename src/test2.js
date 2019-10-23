d42a22c2720b4832957accd1cea0a97d1ef13646
unknown <argam.markosyan1994@gmail.com>
2019-10-24 00:35:42 +0400

    test

--- src/test2.js
+++ src/test2.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 function Test2({ x, y }) {
+
+
+    
     return (
         <div className="Test flex flex-col items-center font-bold text-red">
             <h1>Test2</h1>
